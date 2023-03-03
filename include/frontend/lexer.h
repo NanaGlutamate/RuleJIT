bfa5f359a61fca9f73e36687bff3bad2f441bce4
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2023-03-03 11:21:36 +0800

    lexer small change

--- include/frontend/lexer.h
+++ include/frontend/lexer.h
@@ -13,16 +13,18 @@ public:
         IDENTIFIER = -2,
         SYM = -3,
         KEYWORD = -4,
+        END = -5,
     };
     ExpressionLexer& getLexer(){static ExpressionLexer e; return e;}
-    void loadExpression(const BufferType &expression){buffer = expression;}
-    void loadExpression(BufferType &&expression){buffer = std::move(expression);}
+    void loadExpression(const BufferType &expression){buffer = expression; restartLexer();}
+    void loadExpression(BufferType &&expression){buffer = std::move(expression); restartLexer();}
     ExpressionLexer& operator>>(std::string_view& dst){dst = next(); return *this;}
     ExpressionLexer& operator>>(std::string& dst){dst = next(); return *this;}
     Token tokenType(){return type;}
     std::string_view top();
     std::string_view next();
 private:
+    void restartLexer();
     ExpressionLexer() = default;
     Token type;
     BufferType buffer;
