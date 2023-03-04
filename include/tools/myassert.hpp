0eac79c6c0ef5c04d36728ef57185ed468c09657
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2023-03-04 13:29:08 +0800

    Update myassert.hpp

--- include/tools/myassert.hpp
+++ include/tools/myassert.hpp
@@ -32,7 +32,7 @@ inline void my_assert(bool check,
 inline void my_assert(bool check, std::string message){
     if(!check){
         std::cout << message << std::endl;
-        throw std::logic_error("");
+        throw std::logic_error(message);
     }
 }
 
